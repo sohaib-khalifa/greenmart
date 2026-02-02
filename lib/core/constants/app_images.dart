2dfb0ac1cc9577f4c0012d1018cdb91ceb654cf7
sohaib-khalifa <sohaipkhalifa47@gmail.com>
2026-02-02 21:48:11 +0200

    add AppImages constants

---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,5 @@
+class AppImages {
+  static const String logoSvg = 'assets/images/logo.svg';
+  static const String carrotSvg = 'assets/images/carrot.svg';
+  static const String welcome = 'assets/images/welcome.png';
+}
