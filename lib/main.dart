6e24c3303b1833061806e56a68506454e404094e
sohaib-khalifa <sohaipkhalifa47@gmail.com>
2026-02-02 20:51:02 +0200

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const GreenMart());
+}
+
+class GreenMart extends StatelessWidget {
+  const GreenMart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
