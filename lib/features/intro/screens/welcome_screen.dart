2253b0bb0eb2ec30ba779f345dd823a5a0c66d21
sohaib-khalifa <sohaipkhalifa47@gmail.com>
2026-02-04 09:42:09 +0200

    add welcome screen and reusable primary button

--- lib/features/intro/screens/welcome_screen.dart
+++ lib/features/intro/screens/welcome_screen.dart
@@ -1,10 +1,72 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:greenmart/core/constants/app_images.dart';
+import 'package:greenmart/core/styles/color.dart';
+import 'package:greenmart/core/styles/text_style.dart';
+import 'package:greenmart/features/intro/widgets/primary_button.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            AppImages.welcome,
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Positioned(
+            bottom: 75,
+            left: 30,
+            right: 30,
+            child: Column(
+              children: [
+                SvgPicture.asset(
+                  AppImages.carrotSvg,
+                  colorFilter: ColorFilter.mode(
+                    AppColors.backgroundColor,
+                    BlendMode.srcIn,
+                  ),
+                ),
+                SizedBox(height: 30),
+                Text(
+                  'Welcome\nto our store',
+                  style: TextStyles.headline.copyWith(
+                    color: AppColors.backgroundColor,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  'Ger your groceries in as fast as one hour',
+                  style: TextStyles.body.copyWith(color: AppColors.greyColor),
+                  // maxLines: 1,
+                  // overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.center,
+                  maxLines: 2,
+                ),
+                SizedBox(height: 30),
+
+                PrimaryButton(
+                  onPressed: () {},
+                  title: 'Get Started',
+                  height: 65,
+                  width: double.infinity,
+                  backgroundColor: AppColors.primaryColor,
+                  textStyle: TextStyles.subtitle.copyWith(
+                    color: AppColors.backgroundColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
