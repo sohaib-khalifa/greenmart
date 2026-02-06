64e0e42bc8faecfd9ab05edd6ac69ab4d7c2d3dc
sohaib-khalifa <sohaipkhalifa47@gmail.com>
2026-02-07 00:00:42 +0200

    Add AppFonts, setup global theme for fonts and text buttons, and simplify TextStyles

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:greenmart/core/constants/app_fonts.dart';
 import 'package:greenmart/core/styles/color.dart';
 import 'package:greenmart/features/intro/screens/splash_screen.dart';
 
@@ -12,7 +13,17 @@ class GreenMart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(scaffoldBackgroundColor: AppColors.backgroundColor),
+      theme: ThemeData(
+        fontFamily: AppFonts.poppins,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.primaryColor,
+          onSurface: AppColors.blackColor,
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(foregroundColor: AppColors.blackColor),
+        ),
+        scaffoldBackgroundColor: AppColors.backgroundColor,
+      ),
       builder: (context, child) {
         return SafeArea(top: false, bottom: true, child: child!);
       },
