94f103910dc3507d7d1d7100e162cb732ff956f4
sohaib-khalifa <sohaipkhalifa47@gmail.com>
2026-02-09 04:50:17 +0200

    Add app icon assets for BottomNavigationBar

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -2,4 +2,10 @@ class AppImages {
   static const String logoSvg = 'assets/images/logo.svg';
   static const String carrotSvg = 'assets/images/carrot.svg';
   static const String welcome = 'assets/images/welcome.png';
+
+  static const String storeSvg = 'assets/icons/store.svg';
+  static const String heartSvg = 'assets/icons/heart.svg';
+  static const String userSvg = 'assets/icons/user.svg';
+  static const String exploreSvg = 'assets/icons/explore.svg';
+  static const String cartSvg = 'assets/icons/cart.svg';
 }
