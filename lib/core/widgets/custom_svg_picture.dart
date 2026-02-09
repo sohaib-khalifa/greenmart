c1b9ad5aed7324f6740b1657efb07782bdddcdee
sohaib-khalifa <sohaipkhalifa47@gmail.com>
2026-02-09 05:24:07 +0200

    Temporarily set MainAppScreen as home for faster testing instead of SplashScreen

--- lib/core/widgets/custom_svg_picture.dart
+++ lib/core/widgets/custom_svg_picture.dart
@@ -1,26 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:greenmart/core/constants/app_images.dart';
-import 'package:greenmart/core/styles/color.dart';
 
 class CustomSvgPicture extends StatelessWidget {
   const CustomSvgPicture({
-    super.key, required this.path, this.color,
+    super.key,
+    required this.path,
+    this.color,
+    this.height,
+    this.width,
   });
-final String path;
-final Color? color;
+  final String path;
+  final Color? color;
+  final double? height;
+  final double? width;
 
   @override
   Widget build(BuildContext context) {
     return SvgPicture.asset(
       path,
       // AppImages.storeSvg,
-      colorFilter: 
-      color != null ?
-      ColorFilter.mode(
-        color!,
-        BlendMode.srcIn,
-      ) : null ,
+      colorFilter: color != null
+          ? ColorFilter.mode(color!, BlendMode.srcIn)
+          : null,
+      width: width,
+      height: height,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:greenmart/core/constants/app_fonts.dart';
 import 'package:greenmart/core/styles/color.dart';
-import 'package:greenmart/features/intro/screens/splash_screen.dart';
 import 'package:greenmart/features/main/main_app_screen.dart';
 
 void main() {
@@ -37,8 +36,8 @@ class GreenMart extends StatelessWidget {
         return SafeArea(top: false, bottom: true, child: child!);
       },
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
-      // home: MainAppScreen(),
+      // home: SplashScreen(),
+      home: MainAppScreen(),
     );
   }
 }
