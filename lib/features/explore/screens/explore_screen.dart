2c5ad3cf5c250111eebef74d8cfcde868e5d3c06
sohaib-khalifa <sohaipkhalifa47@gmail.com>
2026-02-16 00:28:06 +0200

    Implement ExploreScreen body with static categories GridView

--- lib/features/explore/screens/explore_screen.dart
+++ lib/features/explore/screens/explore_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:greenmart/core/styles/color.dart';
 import 'package:greenmart/core/styles/text_style.dart';
+import 'package:greenmart/core/widgets/custom_text_form_field.dart';
 
 class ExploreScreen extends StatelessWidget {
   const ExploreScreen({super.key});
@@ -10,6 +12,68 @@ class ExploreScreen extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         title: Text('Find Products', style: TextStyles.subtitle),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Column(
+            spacing: 20,
+            children: [
+              CustomTextFormField(
+                prefixIcon: Icon(Icons.search),
+                hintText: 'Search Store',
+              ),
+              GridView.builder(
+                itemCount: 8,
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisExtent: 180,
+                ),
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 150,
+                    margin: EdgeInsets.symmetric(vertical: 8),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 12,
+                      vertical: 18,
+                    ),
+                    decoration: BoxDecoration(
+                      color: AppColors.backgroundColor,
+                      boxShadow: [
+                        BoxShadow(
+                          color: AppColors.blackColor.withValues(alpha: 0.07),
+                          blurRadius: 10,
+                          offset: Offset(0, 0),
+                          spreadRadius: 0,
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(16),
+
+                      border: Border.all(color: AppColors.accentColor),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(child: Image.asset('assets/images/image.png')),
+
+                        Text(
+                          textAlign: TextAlign.center,
+                          'Frash Fruits & Vegetable',
+                          style: TextStyles.caption.copyWith(
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
