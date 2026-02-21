4d7b0e5de2d66de8423d9984d25c83d9c5e2183f
sohaib-khalifa <sohaipkhalifa47@gmail.com>
2026-02-21 04:21:54 +0200

    Connect FavouriteScreen with ProductModel and display items using FavouriteItemTile

--- lib/features/favourite/screens/favourite_screen.dart
+++ lib/features/favourite/screens/favourite_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:greenmart/core/styles/color.dart';
 import 'package:greenmart/core/styles/text_style.dart';
 import 'package:greenmart/features/favourite/widgets/favourite_item_tile.dart';
+import 'package:greenmart/features/search/data/dummy_data.dart';
 
 class FavouriteScreen extends StatelessWidget {
   const FavouriteScreen({super.key});
@@ -14,9 +15,9 @@ class FavouriteScreen extends StatelessWidget {
         title: Text('Favourite', style: TextStyles.subtitle),
       ),
       body: ListView.separated(
-        itemCount: 10,
+        itemCount: mockSearchDrinksProductsList.length,
         itemBuilder: (context, index) {
-          return FavouriteItemTile();
+          return FavouriteItemTile(productModel: mockSearchDrinksProductsList[index] ,);
         },
         separatorBuilder: (context, index) => const Divider(
           endIndent: 20,
--- lib/features/favourite/widgets/favourite_item_tile.dart
+++ lib/features/favourite/widgets/favourite_item_tile.dart
@@ -1,38 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:greenmart/core/styles/color.dart';
 import 'package:greenmart/core/styles/text_style.dart';
-
-
+import 'package:greenmart/features/home/data/product_model.dart';
 
 class FavouriteItemTile extends StatelessWidget {
-  const FavouriteItemTile({
-    super.key,
-  });
-
+  const FavouriteItemTile({super.key, required this.productModel});
+  final ProductModel productModel;
   @override
   Widget build(BuildContext context) {
     return ListTile(
       contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
       leading: Image.network(
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfDeaMF3DGM6S4lWuPut2CMfBVLtNuPwBtxQ&s',
+        productModel.image,
+        width: 60,
+        height: 60,
+        fit: BoxFit.cover,
       ),
       title: Text(
-        'Item',
+        productModel.name,
         style: TextStyles.body.copyWith(fontWeight: FontWeight.w600),
       ),
-    
+
       subtitle: Padding(
         padding: const EdgeInsets.only(top: 4),
         child: Text(
-          'Description for item',
-          style: TextStyles.small.copyWith(color: AppColors.greyColor),
+          productModel.quantityForPrice,
+          style: TextStyles.small.copyWith(
+            fontWeight: FontWeight.w600,
+            color: AppColors.greyColor,
+          ),
         ),
       ),
       trailing: Row(
         mainAxisSize: MainAxisSize.min,
         children: [
           Text(
-            '\$4.99',
+            '\$${productModel.price}',
             style: TextStyles.caption.copyWith(
               fontWeight: FontWeight.w600,
               color: AppColors.blackColor,
